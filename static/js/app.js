ae6c776d657c5d7d82454162a155fa9aa20bbca6
Bennett Roesch <meta@spyrosoft.com>
2016-01-25 19:37:26 -0800

    Add two audio tracks and key and button events.

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,69 @@
 $(document).foundation();
 
-$( '.beep.button' ).on( 'mousedown', beepBoop );
+var secondsOfToneAudio = 30;
+
+$( '.beep.button' ).on( 'mousedown', startBeepBoop );
 $( window ).on( 'mouseup', stopBeepBoop );
+$( window ).on( 'keydown', checkForCtrlKeydown );
+$( window ).on( 'keyup', checkForCtrlKeyup );
 
-var outputAudio = document.getElementById( 'beep-boop' );
+var beepBoop;
+var alternateBeepBoop;
+var beepBoopTimeinterval;
 
-function beepBoop() {
-	outputAudio.play();
+initializeBeepBoop();
+
+function initializeBeepBoop() {
+	beepBoop = document.getElementById( 'beep-boop-1' );
+	alternateBeepBoop = document.getElementById( 'beep-boop-2' );
+	
+	beepBoop.volume = 0;
+	beepBoop.play();
+	
+	alternateBeepBoop.volume = 0;
+	alternateBeepBoop.play();
+	
+	switchBeepBoop();
+	beepBoopTimeinterval = setInterval(
+		switchBeepBoop,
+		( secondsOfToneAudio * 0.75 ) * 1000
+	);
+}
+
+function startBeepBoop() {
+	beepBoop.volume = 1;
 }
 
 function stopBeepBoop() {
-	outputAudio.pause();
-	outputAudio.currentTime = 0;
+	beepBoop.volume = 0;
+}
+
+function switchBeepBoop() {
+	var beepBoopIds = whichBeepBoopIds();
+	var beepBoopId = beepBoopIds[ 1 ];
+	var alternateBeepBoopId = beepBoopIds[ 0 ];
+	
+	beepBoop = document.getElementById( 'beep-boop-' + beepBoopId );
+	alternateBeepBoop = document.getElementById( 'beep-boop-' + alternateBeepBoopId );
+	
+	beepBoop.currentTime = 0;
+	alternateBeepBoop.volume = 0;
+}
+
+function whichBeepBoopIds() {
+	if ( beepBoop.id === 'beep-boop-1' ) { return [ '1', '2' ]; }
+	return [ '2', '1' ];
+}
+
+
+function checkForCtrlKeydown( keyEvent ) {
+	if ( Utilities.keyCodeLookup( keyEvent ) === 'control' ) {
+		startBeepBoop();
+	}
+}
+
+function checkForCtrlKeyup( keyEvent ) {
+	if ( Utilities.keyCodeLookup( keyEvent ) === 'control' ) {
+		stopBeepBoop();
+	}
 }
